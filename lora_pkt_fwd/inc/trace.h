47f3c0dc8cd788351b9759856ca4b39cd632299a
Anton Petrov <48187945+ZitronenSchrank@users.noreply.github.com>
2023-10-19 17:46:24 +0200

    Added VsCode and Debug Config

--- lora_pkt_fwd/inc/trace.h
+++ lora_pkt_fwd/inc/trace.h
@@ -22,8 +22,8 @@ Maintainer: Michael Coracin
 #define DEBUG_JIT_ERROR 0
 #define DEBUG_TIMERSYNC 0
 #define DEBUG_BEACON    0
-#define DEBUG_LOG       0
-#define DEBUG_INFO      0
+#define DEBUG_LOG       1
+#define DEBUG_INFO      1
 #define DEBUG_PPM       0
 #define DEBUG_WARNING   0
 #define DEBUG_ERROR     1
